42d8258595ef0fafe9ee9a10dbd3667fd3edfb4b
Devous <43449531+Devous@users.noreply.github.com>
2022-01-10 00:52:33 +0000

    Moved package, pushed to location in error

--- src/main/java/com/learnspigot/bot/verification/udemy/course/UdemyCourse.java
+++
@@ -1,6 +0,0 @@
-package com.learnspigot.bot.verification.udemy.course;
-
-import org.jetbrains.annotations.NotNull;
-
-public record UdemyCourse(long id, @NotNull String title, @NotNull String url) {
-}
